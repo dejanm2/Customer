422140b1e59aecace6ab73788d0f428236492817
DeM <dejan.mickovic@gmail.com>
2022-01-09 21:50:39 +0100

    adding initial Kafka dependencies

---
+++ src/main/java/com/logic/customer/controller/KafkaController.java
@@ -0,0 +1,25 @@
+package com.logic.customer.controller;
+
+import com.logic.customer.service.SimpleProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final SimpleProducer producer;
+
+    @Autowired
+    KafkaController(SimpleProducer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
---
+++ src/main/java/com/logic/customer/service/SimpleProducer.java
@@ -0,0 +1,21 @@
+package com.logic.customer.service;
+//https://www.confluent.io/blog/apache-kafka-spring-boot-application/
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleProducer {
+    private static final Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private static final String TOPIC = "users";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+}
